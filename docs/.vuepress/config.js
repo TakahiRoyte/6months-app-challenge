27de03fa52013378900c3f85e19d33c27449c6b1
Takahiro Ito <takahiroito0118@gmail.com>
2019-07-12 17:11:30 +0900

    added '参加する' to navbar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,11 @@ module.exports = {
   description: '6ヶ月でアプリを作ろう',
   themeConfig: {
     nav: [
+      {
+        text: '参加する！',
+        link:
+          'https://forms.office.com/Pages/ResponsePage.aspx?id=LCcCsHdbmkWCx1NZjEqlEZzeFa7W_mhAs_UiUbPq2QxUM1U2MjBCVVpYNTk0N0ZFV0xCMzlZTFdLVS4u',
+      },
       { text: 'どんなアプリを作る？', link: '/examples/' },
       { text: 'ルール', link: '/rules/' },
       { text: 'スケジュール', link: '/schedules/' },
